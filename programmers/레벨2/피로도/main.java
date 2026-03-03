22c2d028b5e322f13efeaaecc69e669b035ecf91
유현종 <guswhd903@naver.com>
2026-03-03 20:16:43 +0900

    [프로그래머스] 피로도 리팩토링

--- programmers/레벨2/피로도/main.java
+++ programmers/레벨2/피로도/main.java
@@ -6,17 +6,17 @@ public class main {
         public int solution(int k, int[][] dungeons) {
             boolean [] isVisited = new  boolean[dungeons.length];
             int answer = -1;
-            dfs(dungeons,k,0,0, isVisited);
+            dfs(dungeons,k,0, isVisited);
             answer = result;
             return answer;
         }
 
-        public void dfs(int [][] dungeons, int curK, int curDungeons, int clear, boolean [] isVisited){
-                result = Math.max(result, clear);
+        public void dfs(int [][] dungeons, int curK, int clear, boolean [] isVisited){
+            result = Math.max(result, clear);
             for (int i = 0; i < dungeons.length; i++) {
                 if(!isVisited[i] && curK >= dungeons[i][0]){
                     isVisited[i] = true;
-                    dfs(dungeons, curK - dungeons[i][1], curDungeons + 1, clear + 1,isVisited);
+                    dfs(dungeons, curK - dungeons[i][1], clear + 1,isVisited);
                     isVisited[i] = false;
                 }
 
