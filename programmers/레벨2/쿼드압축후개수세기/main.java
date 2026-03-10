6d6f4a57e78140f3cf681edef67c8afa0f355f8f
justin <justin@justinui-MacBookAir.local>
2026-03-10 19:30:56 +0900

    [프로그래머스] 쿼드압축후개수세기 리팩토링

--- programmers/레벨2/쿼드압축후개수세기/main.java
+++ programmers/레벨2/쿼드압축후개수세기/main.java
@@ -2,36 +2,32 @@ package 레벨2.쿼드압축후개수세기;
 
 public class main {
     class Solution {
-        int zero = 0;
-        int one = 0;
         public int[] solution(int[][] arr) {
-            int[] answer = new int[2];
-            divide(arr,0,0, arr.length, arr.length);
-            answer[0] = zero;
-            answer[1] = one;
-            return answer;
+            return divide(arr, 0, 0, arr.length);
         }
 
-        public void divide(int [][] arr, int startY, int startX, int endY, int endX){
-            int st = arr[startY][startX];
-            for (int i = startY; i < endY; i++) {
-                for (int j = startX; j < endX ; j++) {
-                    if(st != arr[i][j]){
-                        int midY = (startY + endY) / 2;
-                        int midX = (startX + endX) / 2;
-                        divide(arr, startY, startX, midY, midX);
-                        divide(arr, startY, midX, midY, endX);
-                        divide(arr, midY, startX, endY, midX);
-                        divide(arr, midY, midX, endY, endX);
-                        return;
+        private int[] divide(int[][] arr, int y, int x, int size) {
+            int first = arr[y][x];
+
+            for (int i = y; i < y + size; i++) {
+                for (int j = x; j < x + size; j++) {
+                    if (arr[i][j] != first) {
+                        int half = size / 2;
+
+                        int[] a = divide(arr, y, x, half);
+                        int[] b = divide(arr, y, x + half, half);
+                        int[] c = divide(arr, y + half, x, half);
+                        int[] d = divide(arr, y + half, x + half, half);
+
+                        return new int[] {
+                                a[0] + b[0] + c[0] + d[0],
+                                a[1] + b[1] + c[1] + d[1]
+                        };
                     }
                 }
             }
-            if (st == 1) {
-                one++;
-            } else {
-                zero++;
-            }
+
+            return first == 0 ? new int[]{1, 0} : new int[]{0, 1};
         }
     }
 }
