7af1688e4eb00751f7bafedf4d7b5173783db934
유현종 <guswhd903@naver.com>
2026-03-15 13:58:15 +0900

    [프로그래머스] 방문길이 리팩토링

--- programmers/레벨2/방문길이/main.java
+++ programmers/레벨2/방문길이/main.java
@@ -4,81 +4,37 @@ public class main {
     class Solution {
         public int solution(String dirs) {
             int answer = 0;
-            int [][][] map = new int [11][11][4];
-            int [] now = new int [2];
-            now[0] = 5;
-            now[1] = 5;
-            for (int i = 0; i < dirs.length(); i++) {
-                char dir = dirs.charAt(i);
-                move(map,dir,now);
+            // [현재 y][현재 x][다음 y][다음 x] 형태로 양방향 길을 직관적으로 기록
+            boolean[][][][] visited = new boolean[11][11][11][11];
+
+            int y = 5, x = 5; // 시작 위치
+
+            // charAt 대신 toCharArray()를 사용해 향상된 for문으로 가독성 향상
+            for (char dir : dirs.toCharArray()) {
+                int ny = y;
+                int nx = x;
+
+                if (dir == 'U') ny++;
+                else if (dir == 'D') ny--;
+                else if (dir == 'R') nx++;
+                else if (dir == 'L') nx--;
+
+                // 1. 맵 밖으로 나가는 경우 무시 (코드 간소화)
+                if (ny < 0 || ny > 10 || nx < 0 || nx > 10) continue;
+
+                // 2. 처음 걸어보는 길인지 확인 (양방향 모두 체크)
+                if (!visited[y][x][ny][nx]) {
+                    visited[y][x][ny][nx] = true; // 정방향 방문 처리
+                    visited[ny][nx][y][x] = true; // 역방향 방문 처리
+                    answer++;
+                }
+
+                // 3. 현재 위치 업데이트
+                y = ny;
+                x = nx;
             }
 
-            for (int i = 0; i < 11; i++) {
-                for (int j = 0; j < 11; j++) {
-                    for (int k = 1; k <= 2; k++) {
-                        if(map[i][j][k] == 1){
-                            answer++;
-                        }
-                    }
-                }
-            }
-            // U : 0, R : 1, D : 2, L : 3
             return answer;
-        }
-        public void move(int [][][] map, char dir, int [] now){
-
-            switch(dir){
-                case 'U' : {
-                    int nxtY = now[0] + 1;
-                    int nxtX = now[1];
-
-                    if(nxtY < 0 || nxtY > 10 || nxtX < 0 || nxtX > 10)
-                        break;
-
-                    map[nxtY][nxtX][2] = 1;
-                    now[0] = nxtY;
-                    now[1] = nxtX;
-                    break;
-                }
-                case 'R' : {
-                    int nxtY = now[0];
-                    int nxtX = now[1] + 1;
-
-                    if(nxtY < 0 || nxtY > 10 || nxtX < 0 || nxtX > 10)
-                        break;
-
-                    map[now[0]][now[1]][1] = 1;
-                    now[0] = nxtY;
-                    now[1] = nxtX;
-
-                    break;
-                }
-                case 'D' : {
-                    int nxtY = now[0] - 1;
-                    int nxtX = now[1];
-
-                    if(nxtY < 0 || nxtY > 10 || nxtX < 0 || nxtX > 10)
-                        break;
-
-                    map[now[0]][now[1]][2] = 1;
-                    now[0] = nxtY;
-                    now[1] = nxtX;
-                    break;
-                }
-                case 'L' : {
-                    int nxtY = now[0];
-                    int nxtX = now[1] - 1;
-
-                    if(nxtY < 0 || nxtY > 10 || nxtX < 0 || nxtX > 10)
-                        break;
-
-                    map[nxtY][nxtX][1] = 1;
-                    now[0] = nxtY;
-                    now[1] = nxtX;
-                    break;
-                }
-                default: break;
-            }
         }
     }
 }
