89c6696f9539b0fdba7805aa0d5653aa3c334319
유현종 <guswhd903@naver.com>
2026-03-19 22:22:58 +0900

    [프로그래머스] 소수찾기 리팩토링

--- programmers/레벨2/소수찾기/main.java
+++ programmers/레벨2/소수찾기/main.java
@@ -5,39 +5,12 @@ import java.util.Set;
 
 public class main {
     class Solution {
+        Set<Integer> set = new HashSet<>();
+        boolean[] visited;
+
         public int solution(String numbers) {
-            Set<Integer> set = new HashSet<>();
-
-            for (int pick = 1; pick <= numbers.length(); pick++) {
-                int[] combAry = new int[numbers.length()];
-
-                // 뒤에서부터 pick개를 1로 채움
-                for (int i = numbers.length() - pick; i < numbers.length(); i++) {
-                    combAry[i] = 1;
-                }
-
-                do {
-                    int[] pickedIndex = new int[pick];
-                    int idx = 0;
-
-                    // combAry에서 1인 위치의 인덱스만 뽑기
-                    for (int i = 0; i < numbers.length(); i++) {
-                        if (combAry[i] == 1) {
-                            pickedIndex[idx++] = i;
-                        }
-                    }
-
-                    // 뽑힌 인덱스들의 순열
-                    do {
-                        int num = 0;
-                        for (int i = 0; i < pick; i++) {
-                            num = num * 10 + (numbers.charAt(pickedIndex[i]) - '0');
-                        }
-                        set.add(num);
-                    } while (nextPermutation(pickedIndex));
-
-                } while (nextPermutation(combAry));
-            }
+            visited = new boolean[numbers.length()];
+            dfs(numbers, "");
 
             int answer = 0;
             for (int num : set) {
@@ -45,38 +18,28 @@ public class main {
                     answer++;
                 }
             }
-
             return answer;
         }
 
-        public boolean nextPermutation(int[] arr) {
-            int i = arr.length - 1;
-            while (i > 0 && arr[i - 1] >= arr[i]) i--;
+        private void dfs(String numbers, String current) {
+            if (!current.isEmpty()) {
+                set.add(Integer.parseInt(current));
+            }
 
-            if (i == 0) return false;
+            for (int i = 0; i < numbers.length(); i++) {
+                if (visited[i]) continue;
 
-            int j = arr.length - 1;
-            while (arr[i - 1] >= arr[j]) j--;
-
-            swap(arr, i - 1, j);
-
-            int k = arr.length - 1;
-            while (i < k) swap(arr, i++, k--);
-
-            return true;
+                visited[i] = true;
+                dfs(numbers, current + numbers.charAt(i));
+                visited[i] = false;
+            }
         }
 
-        public void swap(int[] arr, int i, int j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
+        private boolean isPrime(int num) {
+            if (num < 2) return false;
 
-        public boolean isPrime(int n) {
-            if (n < 2) return false;
-
-            for (int i = 2; i * i <= n; i++) {
-                if (n % i == 0) return false;
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) return false;
             }
             return true;
         }
