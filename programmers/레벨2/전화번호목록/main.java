e76c9396c55d88e9aed7d0691e6c759afad822b3
justin <justin@justinui-MacBookAir.local>
2026-03-29 14:27:07 +0900

    [프로그래머스] 전화번호목록 리팩토링

--- programmers/레벨2/전화번호목록/main.java
+++ programmers/레벨2/전화번호목록/main.java
@@ -5,35 +5,15 @@ import java.util.*;
 public class main {
     class Solution {
         public boolean solution(String[] phone_book) {
-            boolean answer = true;
-            boolean [] numberBook = new boolean[21];
-            Arrays.sort(phone_book, Comparator.comparingInt(String::length));
+            Arrays.sort(phone_book);
 
-            Set<String> set = new HashSet<>();
-            List<Integer> slices = new ArrayList<>();
-            for (int i = 0; i < phone_book.length; i++) {
-                String curString = phone_book[i];
-                if(!numberBook[curString.length()]){
-                    numberBook[curString.length()] = true;
-                    slices.add(curString.length());
+            for (int i = 0; i < phone_book.length - 1; i++) {
+                if (phone_book[i + 1].startsWith(phone_book[i])) {
+                    return false;
                 }
-
-                for (int j = 0; j < slices.size(); j++) {
-                    if(slices.get(j) > curString.length()){
-                        break;
-                    }
-                    String substring = curString.substring(0, slices.get(j));
-
-                    if (set.contains(substring)) {
-                        answer = false;
-                        break;
-                    }
-                }
-                set.add(curString);
-
             }
 
-            return answer;
+            return true;
         }
     }
 }
