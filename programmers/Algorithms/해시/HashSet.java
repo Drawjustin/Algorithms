a1fadc20a55243f5c812d1a0d44da7c6f17cfaf6
justin <guswhd903@naver.com>
2026-04-14 17:38:30 +0900

    [알고리즘] HashSet

--- programmers/Algorithms/해시/HashSet.java
+++ programmers/Algorithms/해시/HashSet.java
@@ -1,4 +1,37 @@
 package Algorithms.해시;
 
 public class HashSet {
+    class CustomHashSet<T> {
+        private static final Object PRESENT = new Object();
+
+        private final HashMap.CustomHashMap<T, Object> map;
+
+        CustomHashSet(int capacity) {
+            map = new HashMap().new CustomHashMap<>(capacity);
+        }
+
+        CustomHashSet() {
+            this(16);
+        }
+
+        boolean add(T value) {
+            return map.put(value, PRESENT) == null;
+        }
+
+        boolean contains(T value) {
+            return map.containsKey(value);
+        }
+
+        boolean remove(T value) {
+            return map.remove(value) != null;
+        }
+
+        int size() {
+            return map.size();
+        }
+
+        boolean isEmpty() {
+            return map.isEmpty();
+        }
+    }
 }
